56fc8b796256678b0e15b25578208a609a93f196
hydev777 <hytechwilson@gmail.com>
2024-06-12 15:02:34 -0400

    organized files and class names

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,6 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Animations'),
       ),
       body: Center(
