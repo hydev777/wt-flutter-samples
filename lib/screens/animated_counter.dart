79dcc2c0c33aed674aaa5521751fdeb53ab92e19
hydev777 <hytechwilson@gmail.com>
2025-01-10 18:36:03 -0400

    Updated animated counter

--- lib/screens/animated_counter.dart
+++ lib/screens/animated_counter.dart
@@ -1,4 +1,3 @@
-
 import 'dart:math' show pi;
 
 import 'package:flutter/material.dart';
@@ -14,13 +13,13 @@ class _AnimatedCounterState extends State<AnimatedCounter>
     with SingleTickerProviderStateMixin {
   late final AnimationController _controller = AnimationController(
     vsync: this,
-    duration: const Duration(seconds: 1),
+    duration: const Duration(seconds: 2),
   );
   late final Animation<double> numberChangeValue =
       Tween<double>(begin: 0, end: 1).animate(
     CurvedAnimation(
       parent: _controller,
-      curve: Curves.linear,
+      curve: Curves.bounceOut,
     ),
   );
 
@@ -51,20 +50,21 @@ class _AnimatedCounterState extends State<AnimatedCounter>
       }
 
       if (animationValue > 0.2 && animationValue <= 0.9 && showText) {
-        shadowColor = Colors.blue[700]!;
+        shadowColor = Colors.blue[600]!;
       }
 
       if (animationValue > 0.4 && animationValue <= 0.9 && showText) {
+        shadowColor = Colors.blue[700]!;
+      }
+
+      if (animationValue > 0.462 && animationValue <= 0.9 && showText) {
         shadowColor = Colors.blue[800]!;
+        showShadow = false;
+        showText = false;
       }
 
       if (animationValue > 0.6 && animationValue <= 0.9 && showText) {
-        shadowColor = Colors.blue[900]!;
-      }
-
-      if (animationValue > 1 && animationValue <= 2 && showText) {
-        showShadow = false;
-        showText = false;
+        shadowColor = Colors.black;
       }
     });
   }
@@ -86,99 +86,128 @@ class _AnimatedCounterState extends State<AnimatedCounter>
           child: AnimatedBuilder(
               animation: _controller,
               builder: (context, child) {
-                return SizedBox(
-                  height: 200,
-                  width: 200,
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        bottom: 0,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
+                return Transform(
+                  transform: Matrix4.identity()
+                    ..setEntry(3, 2, 0.002)
+                    ..rotateY(0.2 * pi),
+                  child: SizedBox(
+                    height: 200,
+                    width: 200,
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          bottom: 0,
+                          child: ClipRRect(
+                            borderRadius: const BorderRadius.vertical(
+                              bottom: Radius.circular(10),
+                            ),
+                            child: Container(
+                              height: 100,
+                              width: 200,
+                              decoration: BoxDecoration(
+                                color: shadowColor,
+                                borderRadius: const BorderRadius.vertical(
+                                  bottom: Radius.circular(10),
+                                ),
+                                border: const Border(
+                                  left: BorderSide(color: Colors.black),
+                                  right: BorderSide(color: Colors.black),
+                                  bottom: BorderSide(color: Colors.black),
+                                  top: BorderSide(color: Colors.black),
+                                ),
+                              ),
+                              alignment: Alignment.topCenter,
+                              child: Text(
+                                count.toString(),
+                                style: const TextStyle(
+                                    fontSize: 140, height: 0.001),
+                              ),
+                            ),
+                          ),
+                        ),
+                        ClipRRect(
+                          borderRadius: const BorderRadius.vertical(
+                            top: Radius.circular(10),
+                          ),
                           child: Container(
                             height: 100,
                             width: 200,
-                            decoration: BoxDecoration(
-                              color: shadowColor,
-                              borderRadius: BorderRadius.circular(10),
-                              border: Border.all(
-                                color: Colors.black,
+                            decoration: const BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius: BorderRadius.vertical(
+                                top: Radius.circular(10),
+                              ),
+                              border: Border(
+                                left: BorderSide(color: Colors.black),
+                                right: BorderSide(color: Colors.black),
+                                top: BorderSide(color: Colors.black),
+                                bottom: BorderSide(color: Colors.black),
                               ),
                             ),
-                            alignment: Alignment.topCenter,
-                            child: Text(
-                              count.toString(),
-                              style: const TextStyle(fontSize: 140, height: 0.001),
-                            ),
-                          ),
-                        ),
-                      ),
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Container(
-                          height: 100,
-                          width: 200,
-                          decoration: BoxDecoration(
-                            color: Colors.blue,
-                            borderRadius: BorderRadius.circular(10),
-                            border: Border.all(
-                              color: Colors.black,
-                            ),
-                          ),
-                          alignment: Alignment.bottomCenter,
-                          child: Text(
-                            (count + 1).toString(),
-                            style: const TextStyle(fontSize: 140, height: 0.001),
-                          ),
-                        ),
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          _controller.forward();
-                        },
-                        child: Container(
-                          color: Colors.transparent,
-                          height: 100,
-                          width: 200,
-                          alignment: Alignment.topCenter,
-                          child: Transform(
                             alignment: Alignment.bottomCenter,
-                            transform: Matrix4.identity()
-                              ..setEntry(3, 2, 0.001)
-                              ..rotateX((numberChangeValue.value) * pi),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(10),
-                              child: AnimatedContainer(
-                                duration: const Duration(microseconds: 500),
-                                decoration: BoxDecoration(
-                                  color: showShadow ? shadowColor : Colors.blue,
-                                  borderRadius: BorderRadius.circular(10),
-                                  border: Border.all(
-                                    color: Colors.black,
-                                  ),
+                            child: Text(
+                              (count + 1).toString(),
+                              style:
+                                  const TextStyle(fontSize: 140, height: 0.001),
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            _controller.forward();
+                          },
+                          child: Container(
+                            color: Colors.transparent,
+                            height: 100,
+                            width: 200,
+                            alignment: Alignment.topCenter,
+                            child: Transform(
+                              alignment: Alignment.bottomCenter,
+                              transform: Matrix4.identity()
+                                ..setEntry(3, 2, 0.001)
+                                ..rotateX((numberChangeValue.value) * pi),
+                              child: ClipRRect(
+                                borderRadius: const BorderRadius.vertical(
+                                  top: Radius.circular(10),
                                 ),
-                                height: 100,
-                                width: 200,
-                                alignment: Alignment.bottomCenter,
-                                child: Transform(
-                                  transform: Matrix4.identity()
-                                    ..scale(showText ? 1.0 : -1.0, 1.0, 1.0)
-                                    ..rotateZ(showText ? (pi * 2) : pi),
-                                  alignment: Alignment.center,
-                                  child: Text(
-                                    showText
-                                        ? count.toString()
-                                        : (count + 1).toString(),
-                                    style:
-                                        const TextStyle(fontSize: 140, height: 0.001),
+                                child: AnimatedContainer(
+                                  duration: const Duration(microseconds: 500),
+                                  decoration: BoxDecoration(
+                                    color:
+                                        showShadow ? shadowColor : Colors.blue,
+                                    borderRadius: const BorderRadius.vertical(
+                                      top: Radius.circular(10),
+                                    ),
+                                    border: const Border(
+                                      left: BorderSide(color: Colors.black),
+                                      right: BorderSide(color: Colors.black),
+                                      top: BorderSide(color: Colors.black),
+                                      bottom: BorderSide(color: Colors.black),
+                                    ),
+                                  ),
+                                  height: 100,
+                                  width: 200,
+                                  alignment: Alignment.bottomCenter,
+                                  child: Transform(
+                                    transform: Matrix4.identity()
+                                      ..scale(showText ? 1.0 : -1.0, 1.0, 1.0)
+                                      ..rotateZ(showText ? (pi * 2) : pi),
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      showText
+                                          ? count.toString()
+                                          : (count + 1).toString(),
+                                      style: const TextStyle(
+                                          fontSize: 140, height: 0.001),
+                                    ),
                                   ),
                                 ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 );
               }),
